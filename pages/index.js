c7d3eb0373434d7561adddc90595d1801b5f1386
Max Fierro <elmasse@gmail.com>
2017-09-20 18:10:58 -0300

    emotion

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,65 @@
+
+import { hydrate, injectGlobal } from 'emotion'
+import styled from 'react-emotion'
+
 import withPosts from 'nextein/posts'
 import { Content } from 'nextein/post'
 
 import Link from 'nextein/link'
 
+// Adds server generated styles to emotion cache.
+// '__NEXT_DATA__.ids' is set in '_document.js'
+if (typeof window !== 'undefined') {
+  hydrate(window.__NEXT_DATA__.ids)
+}
+
 export default withPosts(({ posts }) => {
 
+  injectGlobal`
+    html, body {
+      margin: 0;
+    }
+  `
+
   return (
-    <main>
-      <header>
+    <Main>
+      <Header>
         <h1>Chantum</h1>
-      </header>
-      <section>
+      </Header>
+      <Section>
         {
           posts.map( (post, idx) => {
             return (
-              <div key={`post-${idx}`}>
-                <a href={post.data.url}><h2>{post.data.title}</h2></a>
-                {/* <Link {...post} ><a><h2>{post.data.title}</h2></a></Link> */}
+              <Post key={`post-${idx}`}>
+                {/* <a href={post.data.url}><h2>{post.data.title}</h2></a> */}
+                <Link {...post} ><a><h2>{post.data.title}</h2></a></Link>
                 <Content  {...post} excerpt />
-              </div>
+              </Post>
             )
           })
         }
-      </section>
-    </main>
+      </Section>
+    </Main>
   )
 })
+
+
+const Main = styled('main')`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+`
+
+const Header = styled('header')`
+  display: flex;
+  justify-content: center;
+`
+
+const Section = styled('section')`
+  margin: 50px 0;
+`
+
+const Post = styled('div')`
+  max-width: 1000px;
+  margin: 70px auto;
+`
--- pages/post.js
+++ pages/post.js
@@ -1,14 +1,50 @@
+
+import { hydrate, injectGlobal } from 'emotion'
+import styled from 'react-emotion'
+
 import withPost, { Content } from 'nextein/post'
+
+// Adds server generated styles to emotion cache.
+// '__NEXT_DATA__.ids' is set in '_document.js'
+if (typeof window !== 'undefined') {
+  hydrate(window.__NEXT_DATA__.ids)
+}
 
 export default withPost(({ post }) => {
 
+  injectGlobal`
+    html, body {
+      margin: 0;
+    }
+  `
+
   return (
-    <main>
-      <header>
-      <h1>{post.data.title}</h1>
-      </header>
-      <Content {...post} />
-    </main>
+    <Main>
+      <Header>
+        <h1>{post.data.title}</h1>
+      </Header>
+      <Post {...post} />
+    </Main>
   )
 
 })
+
+const Main = styled('main')`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+`
+
+const Header = styled('header')`
+  display: flex;
+  justify-content: center;
+`
+
+const Section = styled('section')`
+  margin: 50px 0;
+`
+
+const Post = styled(Content)`
+  max-width: 1000px;
+  margin: 70px auto;
+`
